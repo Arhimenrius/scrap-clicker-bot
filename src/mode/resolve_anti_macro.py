ab75ec96b41f6c97e93b275484d39b6c3f7a7737
Arhimenrius <arhimenrius@gmail.com>
2018-10-02 03:34:11 +0300

    Move anti-macro resolver to thread as it's heavy process and it was killing bot

--- src/mode/resolve_anti_macro.py
+++ src/mode/resolve_anti_macro.py
@@ -1,128 +1,134 @@
 from src.mobile import Mobile
 from src.digit_detector import detect_digit
 import random
+import threading
+from time import sleep
 import cv2
 
+y_min = 90
+y_max = 503
+x_min = 87
+x_max = 393
 
-class ResolveAntiMacro:
-    mobile = None
+expected_number_y_min = 50
+expected_number_y_max = 70
+expected_number_x_min = 248
+expected_number_x_max = 263
 
-    y_min = 90
-    y_max = 503
-    x_min = 87
-    x_max = 393
+buttons_y_min = 92
+buttons_y_max = 314
+buttons_x_min = 41
+buttons_x_max = 266
 
-    expected_number_y_min = 50
-    expected_number_y_max = 70
-    expected_number_x_min = 248
-    expected_number_x_max = 263
+number_of_buttons_x = 3
+number_of_buttons_y = 3
 
-    buttons_y_min = 92
-    buttons_y_max = 314
-    buttons_x_min = 41
-    buttons_x_max = 266
+button_height = 69
+button_width = 69
+button_offset_x = 9
+button_offset_y = 8
 
-    number_of_buttons_x = 3
-    number_of_buttons_y = 3
+cropped_min_red = 120
+cropped_max_red = 160
+cropped_min_green = 130
+cropped_max_green = 160
+cropped_min_blue = 130
+cropped_max_blue = 160
 
-    button_height = 69
-    button_width = 69
-    button_offset_x = 9
-    button_offset_y = 8
+buttons_min_red = 70
+buttons_max_red = 120
+buttons_min_green = 70
+buttons_max_green = 125
+buttons_min_blue = 70
+buttons_max_blue = 120
 
-    cropped_min_red = 120
-    cropped_max_red = 160
-    cropped_min_green = 130
-    cropped_max_green = 160
-    cropped_min_blue = 130
-    cropped_max_blue = 160
+# X/Y per row
+buttons_position_on_mobile = [
+    # ROW 1
+    [[315, 650], [540, 650], [780, 650]],
+    # ROW 2
+    [[315, 875], [540, 875], [780, 875]],
+    # ROW 3
+    [[315, 1100], [540, 1100], [780, 1100]],
+]
 
-    buttons_min_red = 70
-    buttons_max_red = 120
-    buttons_min_green = 70
-    buttons_max_green = 125
-    buttons_min_blue = 70
-    buttons_max_blue = 120
+confirm_button = [530, 1350]
 
-    # X/Y per row
-    buttons_position_on_mobile = [
-        # ROW 1
-        [[315, 650], [540, 650], [780, 650]],
-        # ROW 2
-        [[315, 875], [540, 875], [780, 875]],
-        # ROW 3
-        [[315, 1100], [540, 1100], [780, 1100]],
-    ]
+mobile = Mobile()
 
-    confirm_button = [530, 1350]
 
-    ap = None
-
-    def __init__(self):
-        self.mobile = Mobile()
-
+class DetectAntiMacro:
     def is_mode_active(self, frame):
-        cropped = self.crop_original_frame(frame)
+        cropped = crop_original_frame(frame)
         buttons_to_click = cropped[
-                           self.buttons_y_min:self.buttons_y_max,
-                           self.buttons_x_min:self.buttons_x_max
+                           buttons_y_min:buttons_y_max,
+                           buttons_x_min:buttons_x_max
                            ]
         average_color_of_cropped = [cropped[:, :, i].mean() for i in range(cropped.shape[-1])]
         average_color_of_buttons_to_click = [buttons_to_click[:, :, i].mean() for i in range(cropped.shape[-1])]
-        if self.cropped_min_blue < average_color_of_cropped[0] < self.cropped_max_blue \
-                and self.cropped_min_green < average_color_of_cropped[1] < self.cropped_max_green \
-                and self.cropped_min_red < average_color_of_cropped[2] < self.cropped_max_red \
-                and self.buttons_min_blue < average_color_of_buttons_to_click[0] < self.buttons_max_blue \
-                and self.buttons_min_green < average_color_of_buttons_to_click[1] < self.buttons_max_green \
-                and self.buttons_min_red < average_color_of_buttons_to_click[2] < self.buttons_max_red:
+        if cropped_min_blue < average_color_of_cropped[0] < cropped_max_blue \
+                and cropped_min_green < average_color_of_cropped[1] < cropped_max_green \
+                and cropped_min_red < average_color_of_cropped[2] < cropped_max_red \
+                and buttons_min_blue < average_color_of_buttons_to_click[0] < buttons_max_blue \
+                and buttons_min_green < average_color_of_buttons_to_click[1] < buttons_max_green \
+                and buttons_min_red < average_color_of_buttons_to_click[2] < buttons_max_red:
             return True
         return False
 
-    def process_mode(self, frame):
-        cropped = self.crop_original_frame(frame)
+
+class ResolveAntiMacro(threading.Thread):
+    frame = None
+
+    def __init__(self, frame):
+        self.frame = frame
+        threading.Thread.__init__(self)
+
+    def run(self):
+        cropped = crop_original_frame(self.frame)
 
         expected_number_image = cropped[
-                                self.expected_number_y_min:self.expected_number_y_max,
-                                self.expected_number_x_min:self.expected_number_x_max
+                                expected_number_y_min:expected_number_y_max,
+                                expected_number_x_min:expected_number_x_max
                                 ]
         try:
             expected_number = int(detect_digit(expected_number_image)[0])
             numbers_to_click = cropped[
-                               self.buttons_y_min:self.buttons_y_max,
-                               self.buttons_x_min:self.buttons_x_max
+                               buttons_y_min:buttons_y_max,
+                               buttons_x_min:buttons_x_max
                                ]
 
-            for row in range(0, self.number_of_buttons_y):
-                for column in range(0, self.number_of_buttons_x):
+            for row in range(0, number_of_buttons_y):
+                for column in range(0, number_of_buttons_x):
                     button = numbers_to_click[
-                             (self.button_height * row) + (self.button_offset_y * row):
-                             (self.button_height * (row + 1)) + (self.button_offset_y * row),
-                             (self.button_width * column) + (self.button_offset_x * column):
-                             (self.button_width * (column + 1)) + (self.button_offset_x * column),
+                             (button_height * row) + (button_offset_y * row):
+                             (button_height * (row + 1)) + (button_offset_y * row),
+                             (button_width * column) + (button_offset_x * column):
+                             (button_width * (column + 1)) + (button_offset_x * column),
                              ]
                     number = int(detect_digit(button)[0])
 
                     if number == expected_number:
                         pass
-                        x = self.buttons_position_on_mobile[row][column][0]
-                        y = self.buttons_position_on_mobile[row][column][1]
-                        self.mobile.initTouch()
-                        self.mobile.actionDuringTouch(
+                        x = buttons_position_on_mobile[row][column][0]
+                        y = buttons_position_on_mobile[row][column][1]
+                        mobile.initTouch()
+                        mobile.actionDuringTouch(
                             x + random.randint(20, 40),
                             y + random.randint(20, 40),
                         )
-                        self.mobile.clearTouch()
-
-            self.mobile.initTouch()
-            self.mobile.actionDuringTouch(
-                self.confirm_button[0] + random.randint(20, 40),
-                self.confirm_button[1] + random.randint(20, 40),
+                        mobile.clearTouch()
+            sleep(0.5)
+            mobile.initTouch()
+            mobile.actionDuringTouch(
+                confirm_button[0] + random.randint(20, 40),
+                confirm_button[1] + random.randint(20, 40),
             )
-            self.mobile.clearTouch()
+            mobile.clearTouch()
         except ValueError:
             pass
         except IndexError:
             pass
 
-    def crop_original_frame(self, frame):
-        return frame[self.y_min:self.y_max, self.x_min:self.x_max]
+
+def crop_original_frame(frame):
+    return frame[y_min:y_max, x_min:x_max]
--- src/mode_controller.py
+++ src/mode_controller.py
@@ -2,10 +2,10 @@ from src.video_stream import VideoStream
 from src.mode.collect_magnet_cloud import CollectMagnetCloud
 from src.mode.collect_steel import CollectSteel
 from src.mode.merge import Merge
+from src.mode.resolve_anti_macro import DetectAntiMacro
 from src.mode.resolve_anti_macro import ResolveAntiMacro
 import cv2
-import time
-import logging
+from time import sleep
 
 test = 'default'
 
@@ -17,26 +17,23 @@ class ModeController:
     collectMagnetCloudMode = None
     collectSteelMode = None
     mergeMode = None
-    resolveAntiMacroMode = None
+    detect_anti_macro_mode = None
 
-    number_of_frames_to_skip_for_merge = 30
-    skipped_frames_for_merge = 0
-    number_of_frames_to_skip_after_anti_macro = 120
-    skipped_frames_after_anti_macro = 0
+    number_of_frames_to_skip = 30
+    skipped_frames = 0
 
     def __init__(self):
         self.streamingUri = 'tcp://127.0.0.1:' + self.portForStreaming
         self.collectMagnetCloudMode = CollectMagnetCloud()
         self.collectSteelMode = CollectSteel()
         self.mergeMode = Merge()
-        self.resolveAntiMacroMode = ResolveAntiMacro()
+        self.detect_anti_macro_mode = DetectAntiMacro()
 
     def control(self):
         stream = VideoStream(self.portForStreaming)
         stream.start()
-        time.sleep(1)
         camera = cv2.VideoCapture(self.streamingUri)
-        anti_macro_resolved = False
+        resolver = None
 
         while True:
             pass
@@ -46,28 +43,28 @@ class ModeController:
                 cv2.destroyAllWindows()
                 camera = cv2.VideoCapture(self.streamingUri)
                 continue
-            cv2.imshow('test', frame)
-            cv2.waitKey(1)
-            if anti_macro_resolved:
-                if self.skipped_frames_after_anti_macro < self.number_of_frames_to_skip_after_anti_macro:
-                    self.skipped_frames_after_anti_macro = self.skipped_frames_after_anti_macro + 1
+            if self.detect_anti_macro_mode.is_mode_active(frame):
+                if self.skipped_frames != self.number_of_frames_to_skip:
+                    self.skipped_frames = self.skipped_frames + 1
                     continue
                 else:
-                    self.skipped_frames_after_anti_macro = 0
-                    anti_macro_resolved = False
+                    self.skipped_frames = 0
+                    if resolver is None:
+                        resolver = ResolveAntiMacro(frame)
+                        resolver.start()
+                    else:
+                        if not resolver.isAlive():
+                            resolver = None
 
-            if self.resolveAntiMacroMode.is_mode_active(frame):
-                self.resolveAntiMacroMode.process_mode(frame)
-                anti_macro_resolved = True
             elif self.collectMagnetCloudMode.is_mode_active(frame):
                 self.collectMagnetCloudMode.process_mode()
             elif self.collectSteelMode.is_mode_active(frame):
                 self.collectSteelMode.process_mode()
             else:
-                if self.skipped_frames_for_merge != self.number_of_frames_to_skip_for_merge:
-                    self.skipped_frames_for_merge = self.skipped_frames_for_merge + 1
+                if self.skipped_frames != self.number_of_frames_to_skip:
+                    self.skipped_frames = self.skipped_frames + 1
                     continue
                 # in other case, process it
                 else:
-                    self.skipped_frames_for_merge = 0
+                    self.skipped_frames = 0
                     self.mergeMode.process_mode(frame)
