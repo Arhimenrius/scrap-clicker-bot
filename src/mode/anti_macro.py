069e79b7c0c9c66e25ac5c2d309ded62eaa5daaa
Arhimenrius <arhimenrius@gmail.com>
2018-10-11 12:19:56 +0300

    Upgrade anti-macro

--- src/mode/anti_macro.py
+++ src/mode/anti_macro.py
@@ -1,18 +1,20 @@
 import src.mobile as mobile
-from src.digit_detector import detect_digit
-import random
+import cv2
 import threading
 from time import sleep
+import numpy as np
+import random
+from skimage.measure import compare_ssim as ssim
 
 y_min = 90
 y_max = 503
 x_min = 87
 x_max = 393
 
-expected_number_y_min = 50
-expected_number_y_max = 70
-expected_number_x_min = 248
-expected_number_x_max = 263
+expected_number_y_min = 38
+expected_number_y_max = 86
+expected_number_x_min = 251
+expected_number_x_max = 286
 
 buttons_y_min = 92
 buttons_y_max = 314
@@ -27,48 +29,80 @@ button_width = 69
 button_offset_x = 9
 button_offset_y = 8
 
-cropped_min_red = 120
-cropped_max_red = 160
-cropped_min_green = 130
-cropped_max_green = 160
-cropped_min_blue = 130
-cropped_max_blue = 160
+event_info_min_red = 95
+event_info_max_red = 105
+event_info_min_green = 110
+event_info_max_green = 120
+event_info_min_blue = 120
+event_info_max_blue = 130
 
-buttons_min_red = 70
-buttons_max_red = 120
-buttons_min_green = 70
-buttons_max_green = 125
-buttons_min_blue = 70
-buttons_max_blue = 120
+confirm_button_row_min_red = 125
+confirm_button_row_max_red = 140
+confirm_button_row_min_green = 165
+confirm_button_row_max_green = 175
+confirm_button_row_min_blue = 195
+confirm_button_row_max_blue = 205
 
-# X/Y per row
-buttons_position_on_mobile = [
+tiles_coordinates = [
     # ROW 1
-    [[315, 650], [540, 650], [780, 650]],
+    [41, 92],
+    [119, 92],
+    [197, 92],
     # ROW 2
-    [[315, 875], [540, 875], [780, 875]],
+    [41, 168],
+    [119, 168],
+    [197, 168],
     # ROW 3
-    [[315, 1100], [540, 1100], [780, 1100]],
+    [41, 245],
+    [119, 245],
+    [197, 245],
 ]
 
+# X/Y per row
+tiles_position_on_mobile = [
+    # ROW 1
+    [315, 650],
+    [540, 650],
+    [780, 650],
+    # ROW 2
+    [315, 875],
+    [540, 875],
+    [780, 875],
+    # ROW 3
+    [315, 1100],
+    [540, 1100],
+    [780, 1100],
+]
+
+tile_size = 69
+
 confirm_button = [530, 1350]
+
+event_info_x = [87, 393]
+event_info_y = [90, 128]
+
+confirm_button_row_x = [87, 393]
+confirm_button_row_y = [428, 474]
 
 
 class DetectAntiMacro:
     def is_mode_active(self, frame):
-        cropped = crop_original_frame(frame)
-        buttons_to_click = cropped[
-                           buttons_y_min:buttons_y_max,
-                           buttons_x_min:buttons_x_max
-                           ]
-        average_color_of_cropped = [cropped[:, :, i].mean() for i in range(cropped.shape[-1])]
-        average_color_of_buttons_to_click = [buttons_to_click[:, :, i].mean() for i in range(cropped.shape[-1])]
-        if cropped_min_blue < average_color_of_cropped[0] < cropped_max_blue \
-                and cropped_min_green < average_color_of_cropped[1] < cropped_max_green \
-                and cropped_min_red < average_color_of_cropped[2] < cropped_max_red \
-                and buttons_min_blue < average_color_of_buttons_to_click[0] < buttons_max_blue \
-                and buttons_min_green < average_color_of_buttons_to_click[1] < buttons_max_green \
-                and buttons_min_red < average_color_of_buttons_to_click[2] < buttons_max_red:
+        event_info = frame[
+                     event_info_y[0]:event_info_y[1],
+                     event_info_x[0]:event_info_x[1]
+                     ]
+        confirm_button_row = frame[
+                             confirm_button_row_y[0]:confirm_button_row_y[1],
+                             confirm_button_row_x[0]:confirm_button_row_x[1]
+                             ]
+        event_info_avg_color = [event_info[:, :, i].mean() for i in range(event_info.shape[-1])]
+        confirm_button_row_avg_color = [confirm_button_row[:, :, i].mean() for i in range(confirm_button_row.shape[-1])]
+        if event_info_min_blue < event_info_avg_color[0] < event_info_max_blue \
+                and event_info_min_green < event_info_avg_color[1] < event_info_max_green \
+                and event_info_min_red < event_info_avg_color[2] < event_info_max_red \
+                and confirm_button_row_min_blue < confirm_button_row_avg_color[0] < confirm_button_row_max_blue \
+                and confirm_button_row_min_green < confirm_button_row_avg_color[1] < confirm_button_row_max_green \
+                and confirm_button_row_min_red < confirm_button_row_avg_color[2] < confirm_button_row_max_red:
             return True
         return False
 
@@ -81,50 +115,51 @@ class ResolveAntiMacro(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        cropped = crop_original_frame(self.frame)
+        anti_macro_popup = crop_original_frame(self.frame)
 
-        expected_number_image = cropped[
-                                expected_number_y_min:expected_number_y_max,
-                                expected_number_x_min:expected_number_x_max
-                                ]
-        try:
-            expected_number = int(detect_digit(expected_number_image)[0])
-            numbers_to_click = cropped[
-                               buttons_y_min:buttons_y_max,
-                               buttons_x_min:buttons_x_max
-                               ]
+        target = cv2.cvtColor(cv2.resize(anti_macro_popup[
+                              expected_number_y_min:expected_number_y_max,
+                              expected_number_x_min:expected_number_x_max
+                              ], (69, 69)), cv2.COLOR_BGR2GRAY)
 
-            for row in range(0, number_of_buttons_y):
-                for column in range(0, number_of_buttons_x):
-                    button = numbers_to_click[
-                             (button_height * row) + (button_offset_y * row):
-                             (button_height * (row + 1)) + (button_offset_y * row),
-                             (button_width * column) + (button_offset_x * column):
-                             (button_width * (column + 1)) + (button_offset_x * column),
-                             ]
-                    number = int(detect_digit(button)[0])
+        tiles = [
+            cv2.cvtColor(
+                anti_macro_popup[
+                p[1]:p[1] + tile_size,
+                p[0]:p[0] + tile_size
+                ],
+                cv2.COLOR_BGR2GRAY
+            ) for p in tiles_coordinates
+        ]
+        # print(distances)
+        for i in range(len(tiles)):
+            m = self.mse (target, tiles[i])
+            # s = ssim(target, tiles[i])
+            if m < 5610:
+                mobile.initTouch()
+                mobile.actionDuringTouch(
+                    tiles_position_on_mobile[i][0] + random.randint(20, 40),
+                    tiles_position_on_mobile[i][1] + random.randint(20, 40),
+                )
+                mobile.clearTouch()
+        sleep(0.1)
+        mobile.initTouch()
+        mobile.actionDuringTouch(
+            confirm_button[0] + random.randint(20, 40),
+            confirm_button[1] + random.randint(20, 40),
+        )
+        mobile.clearTouch()
 
-                    if number == expected_number:
-                        pass
-                        x = buttons_position_on_mobile[row][column][0]
-                        y = buttons_position_on_mobile[row][column][1]
-                        mobile.initTouch()
-                        mobile.actionDuringTouch(
-                            x + random.randint(20, 40),
-                            y + random.randint(20, 40),
-                        )
-                        mobile.clearTouch()
-            sleep(0.5)
-            mobile.initTouch()
-            mobile.actionDuringTouch(
-                confirm_button[0] + random.randint(20, 40),
-                confirm_button[1] + random.randint(20, 40),
-            )
-            mobile.clearTouch()
-        except ValueError:
-            pass
-        except IndexError:
-            pass
+    def mse(self, target, tile):
+        # the 'Mean Squared Error' between the two images is the
+        # sum of the squared difference between the two images;
+        # NOTE: the two images must have the same dimension
+        err = np.sum((target.astype("float") - tile.astype("float")) ** 2)
+        err /= float(target.shape[0] * target.shape[1])
+
+        # return the MSE, the lower the error, the more "similar"
+        # the two images are
+        return err
 
 
 def crop_original_frame(frame):
